188002ae6bfe1789067bb112c271eabb4013c7a4
Ascol57 <ascol57@yahoo.com>
2023-12-08 19:58:03 +0100

    Republishing the repo

---
+++ index.js
@@ -0,0 +1,79 @@
+const xml2js = require('xml2js');
+const fs = require('fs');
+const Instrument = require('./instruments');
+const partInfo = require('./partInfo');
+const Measure = require('./measure');
+
+class XMLParser {
+    constructor() {
+        this.parser = new xml2js.Parser({ attrkey: "ATTR" });
+        this.builder = new xml2js.Builder({ attrkey: "ATTR" });
+        this.xml = null;
+        this.json = null;
+        this.Dfile = {
+            instruments: []
+        };
+    }
+
+    async buildxml() {
+        this.xml = await this.builder.buildObject(this.json);
+        this.xml = '<?xml version="1.0" encoding="UTF-8"?>\n<!DOCTYPE score-partwise PUBLIC "-//Recordare//DTD MusicXML 4.0 Partwise//EN" "http://www.musicxml.org/dtds/partwise.dtd">' + this.xml.split('<?xml version="1.0" encoding="UTF-8" standalone="yes"?>')[1]
+        return this.xml
+    }
+
+    async reloadjson() {
+        this.Dfile.instruments.forEach((instrument, index) => {
+            this.json["score-partwise"]["part-list"][0]["score-part"][index] = instrument.toJSON()
+        });
+
+        this.json["score-partwise"] = {
+            ...this.json["score-partwise"],
+            ...this.Dfile.infos.toJSON()
+        };
+
+        var part = []
+        const parts = this.Dfile.parts
+        Object.keys(this.Dfile.parts).forEach(function (k) {
+            console.log(parts[k])
+            part.push({
+                ATTR: { id: k },
+                measure: parts[k]
+            })
+        });
+        
+
+
+        return this.json
+    }
+
+    async loadFile(file) {
+        file = fs.readFileSync(file, 'utf8');
+        this.xml = file;
+        this.json = await this.parser.parseStringPromise(this.xml);
+        await this.reloadDebbusyFile();
+        return this.json
+    }
+
+    reloadDebbusyFile() {
+        this.json["score-partwise"]["part-list"][0]["score-part"].forEach(element => {
+            this.Dfile.instruments.push(new Instrument(element))
+        })
+        this.Dfile.infos = new partInfo(this.json["score-partwise"])
+        this.Dfile.parts = {}
+        this.json["score-partwise"]["part"].forEach(element => {
+            var measures = []
+            element.measure.forEach((element) => {
+                measures.push(new Measure(element))
+            })
+            this.Dfile.parts[element.ATTR.id] = element.measure
+        })
+    }
+
+    get(type) {
+        if (type == "instrument") {
+            return this.Dfile
+        }
+    }
+}
+
+module.exports = XMLParser;
---
+++ measure.js
@@ -0,0 +1,118 @@
+class Measure {
+    constructor(content) {
+        this.notes = []
+        if (content.attributes) {
+            var attributes = {}
+
+            if (content.attributes[0].divisions) {
+                attributes.divisions = content.attributes[0].divisions[0]
+            }
+            if (content.attributes[0].key) {
+                attributes.key = {
+                    fifths: content.attributes[0].key[0].fifths[0]
+                }
+            }
+            if (content.attributes[0].time) {
+                attributes.time = {
+                    beats: content.attributes[0].time[0].beats[0],
+                    'beat-type': content.attributes[0].time[0]["beat-type"][0]
+                }
+            }
+            if (content.attributes[0].clef) {
+                attributes.clef = {
+                    sign: content.attributes[0].clef[0].sign[0],
+                    line: content.attributes[0].clef[0].line[0]
+                }
+            }
+
+            this.attributes = attributes
+        }
+
+        if (content.barline) {
+            var barline = {}
+
+            if (content.barline[0].ATTR.location) {
+                barline.location = content.barline[0].ATTR.location
+            }
+            if (content.barline[0]["bar-style"]) {
+                barline.style = content.barline[0]["bar-style"][0]
+            }
+
+            this.barline = barline
+        }
+
+        content.note.forEach(note => {
+            note = {
+                pitch: {
+                    step: note.pitch[0].step[0],
+                    octave: note.pitch[0].octave[0]
+                },
+                duration: note.duration,
+                type: note.type,
+                voice: note.voice,
+                stem: note.stem
+            }
+
+            var notemod = {}
+
+            Object.keys(note).forEach(function (k) {
+                if (k != "pitch") {
+                    if (note[k]) {
+                        notemod[k] = note[k][0]
+                    }
+                } else {
+                    notemod[k] = note[k]
+                }
+            });
+
+            this.notes.push(notemod)
+        })
+    }
+
+    toJSON() {
+        var json = {
+            "ATTR": {
+                "id": ""
+            },
+            "part-name": [""],
+            "part-abbreviation": [""],
+            "score-instrument": [{
+                "ATTR": {
+                    "id": ""
+                },
+                "instrument-name": [""]
+            }],
+            "midi-instrument": [{
+                "ATTR": {
+                    "id": ""
+                },
+                "midi-channel": [""],
+                "midi-program": [""],
+                "volume": [""],
+                "pan": [""]
+            }],
+            "midi-device": [{
+                "ATTR": {
+                    "id": "",
+                    "port": ""
+                }
+            }]
+        }
+        json["ATTR"]["id"] = this.id
+        json["part-name"][0] = this.name
+        json["part-abbreviation"][0] = this.abbreviation
+        json["score-instrument"][0]["ATTR"]["id"] = this.scoreInstrument.id
+        json["score-instrument"][0]["instrument-name"][0] = this.scoreInstrument.name
+        json["midi-instrument"][0]["ATTR"]["id"] = this.midiInstrument.id
+        json["midi-instrument"][0]["midi-channel"][0] = this.midiInstrument.name
+        json["midi-instrument"][0]["midi-program"][0] = this.midiInstrument.abbreviation
+        json["midi-instrument"][0]["volume"][0] = this.midiInstrument.volume
+        json["midi-instrument"][0]["pan"][0] = this.midiInstrument.pan
+        json["midi-device"][0]["ATTR"]["id"] = this.midiDevice.id
+        json["midi-device"][0]["ATTR"]["port"] = this.midiDevice.port
+
+        return json
+    }
+}
+
+module.exports = Measure;
